4eadc0633e999f2c9aedd2f9b83e3034e7be2f2b
Bledhard <1akaant1@gmail.com>
2019-01-05 14:53:39 +0200

    feat(Structs): Add examples for the Rust Book #5.2
    Files within structs folder cover 'An Example Program Using Structs'
    paragraph of the Rust Book

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,5 +1,7 @@
 mod struct_basics;
+mod struct_example;
 
 pub fn struct_examples() {
     struct_basics::examples();
+    struct_example::example();
 }
---
+++ src/structs/struct_example/mod.rs
@@ -0,0 +1,15 @@
+mod version_1;
+mod version_2;
+mod version_3;
+mod version_4;
+
+pub fn example() {
+    println!("\nExample #1:");
+    version_1::example();
+    println!("\nExample #2:");
+    version_2::example();
+    println!("\nExample #3:");
+    version_3::example();
+    println!("\nExample #4:");
+    version_4::example();
+}
