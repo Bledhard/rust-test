f7499af9531cc81e66fd4f71a450d94596a22ae1
Bledhard <1akaant1@gmail.com>
2019-01-12 16:42:47 +0200

    feat(collection): Add examples for Vector usage
    This commit covers paragraph 8.1 of the Rust Book

---
+++ src/collections/vectors/mod.rs
@@ -0,0 +1,54 @@
+pub fn examples() {
+
+}
+
+fn example1() {
+    let v: Vec<i32> = Vec::new();
+}
+
+fn example2() {
+    let v = vec![1,2,3];
+} // <- v goes out of scope and is freed here
+
+fn example3() {
+    let mut v = Vec::new();
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+}
+
+fn example4() {
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    let third: Option<&i32> = v.get(2);
+}
+
+fn example5() {
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+}
+
+fn example6() {
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+}
+
+fn example7() {
+    let row = vec![
+    SpreadsheetCell::Int(3),
+    SpreadsheetCell::Text(String::from("blue")),
+    SpreadsheetCell::Float(10.12),
+    ];
+}
+
+enum SpreadsheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
