e45e47731d015f3a5f9800c701de2eef7a91866b
Bledhard <1akaant1@gmail.com>
2019-01-21 22:30:44 +0200

    feat(error_handling): Add examples of if panic! usages
    This commit covers paragraph 9.3 of the Rust book.

---
+++ src/error_handling/panic_detail.rs
@@ -0,0 +1,25 @@
+use std::net::IpAddr;
+
+fn example1() {
+    let home: IpAddr = "127.0.0.1".parse().unwrap();
+}
+
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
