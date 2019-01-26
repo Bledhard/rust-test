0c1fd43ab45d547f86e3de44a7f9fd5ee3a35825
Bledhard <1akaant1@gmail.com>
2019-01-26 18:49:47 +0200

    feat(generics): Add examples of Lifetimes
    This commit covers paragraph 10.3 of the Rust book.

---
+++ src/generics/lifetimes.rs
@@ -0,0 +1,85 @@
+// pub fn example1() {
+//     let r;                // ---------+-- 'a
+//                           //          |
+//     {                     //          |
+//         let x = 5;        // -+-- 'b  |
+//         r = &x;           //  |       |
+//     }                     // -+       |
+//                           //          |
+//     println!("r: {}", r); //          |
+// }                         // ---------+   
+
+pub fn example2() {
+    let x = 5;            // ----------+-- 'b
+                          //           |
+    let r = &x;           // --+-- 'a  |
+                          //   |       |
+    println!("r: {}", r); //   |       |
+                          // --+       |
+}                         // ----------+
+
+pub fn example3() {
+    &i32        // a reference
+    &'a i32     // a reference with an explicit lifetime
+    &'a mut i32 // a mutable reference with an explicit lifetime
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+pub fn example4() {
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+pub fn example5() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+pub fn example6() {
+    let s: &'static str = "I have a static lifetime.";
+}
+
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
