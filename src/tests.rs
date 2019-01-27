d9fa86fc6ac2802a769ef28a99a3fbd60f702731
Bledhard <1akaant1@gmail.com>
2019-01-27 16:12:32 +0200

    feat(tests): Add examples for Writing Tests
    This commit covers paragraph 11.1 of the Rust book.

---
+++ src/tests.rs
@@ -0,0 +1,84 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+    
+    #[derive(Debug)]
+    pub struct Rectangle {
+        length: u32,
+        width: u32,
+    }
+
+    impl Rectangle {
+        pub fn can_hold(&self, other: &Rectangle) -> bool {
+            self.length > other.length && self.width > other.width
+        }
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle { length: 8, width: 7 };
+        let smaller = Rectangle { length: 5, width: 1 };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    pub fn add_two(a: i32) -> i32 {
+        a + 2
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    pub fn greeting(name: &str) -> String {
+        format!("Hello {}!", name)
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`", result
+        );
+    }
+
+    pub struct Guess {
+        value: u32,
+    }
+
+    impl Guess {
+        pub fn new(value: u32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {}.", value);
+            }
+
+            Guess {
+                value
+            }
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
