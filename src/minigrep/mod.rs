0df35f6263158da862d4d2e23ea99f0afa3e3e08
Bledhard <1akaant1@gmail.com>
2019-02-05 23:13:44 +0200

    feat(minigrep): Change println mechanic
    This commit covers paragraph 12.6 of the Rust book.

--- src/minigrep/mod.rs
+++ src/minigrep/mod.rs
@@ -10,12 +10,12 @@ pub fn startup() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
 
     if let Err(e) = file::read(&config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
