750db02d712e1ed1b4caf094e4710310bf9e2434
Bledhard <1akaant1@gmail.com>
2019-02-13 22:18:48 +0200

    feat(pointer): Add example of Cons list
    This commit ends up the 15.1 paragraph of the Rust book.

--- src/pointers/mod.rs
+++ src/pointers/mod.rs
@@ -2,3 +2,17 @@ fn example1() {
     let b = Box::new(5);
     println!("b = {}", b);
 }
+
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+fn main() {
+    let list = Cons(1,
+        Box::new(Cons(2,
+            Box::new(Cons(3,
+                Box::new(Nil))))));
+}
