414fe8c66751db09507941e4c06a626dc10327fa
Bledhard <1akaant1@gmail.com>
2019-02-14 22:03:27 +0200

    feat(pointers): Add example of Deref trait
    This commit covers paragraph 15.2 of the Rust book.

---
+++ src/pointers/deref.rs
@@ -0,0 +1,41 @@
+fn example1() {
+    let x = 5;
+    let y = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+fn example2() {    
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+fn example3() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
+
+use std::ops::Deref;
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
